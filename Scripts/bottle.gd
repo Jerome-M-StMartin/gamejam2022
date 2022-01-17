e5e0ec57a741e3c8f3fc9ba8b29e77ac9058a48a
Jerome M. St.Martin <jeromemst.martin@gmail.com>
2022-01-17 14:03:44 -0800

    bird now bobs, + other stuff

--- Scripts/bottle.gd
+++ Scripts/bottle.gd
@@ -1,15 +1,28 @@
-extends Node2D
+extends RigidBody2D
+
+export var G = Vector2(0, 9.8)
+
+signal bottle_pickup
 
 func _ready():
+	contact_monitor = true
+	contacts_reported = 3
+	gravity_scale = 0
 	var message: Node = Node.new()
 	message.name = "Message"
 	add_child(message, true)
 
-func _process(delta):
-	print($Message)
+func _integrate_forces(_state):
+	if position.y >= get_viewport().size.y - 50:
+		linear_damp = 100
+
+func _physics_process(_delta):
+	.apply_central_impulse(G)
 
 func on_message_delivered():
 	pass
 
-func on_pickup():
-	pass
+func _on_Bottle_body_entered(body):
+	if body.name == "Pidgeon":
+		emit_signal("bottle_pickup")
+	self.queue_free()
--- Scripts/pidgeon.gd
+++ Scripts/pidgeon.gd
@@ -1,11 +1,11 @@
 extends RigidBody2D
 
+onready var amplitude = Vector2(0, -15)
+var tween_start_pos;
+
 export var G = Vector2(0, 9.8)
 export var FLAP_VEC = Vector2(0, -300)
 export var MAX_VEL = 500
-
-enum SpriteMode { UNLADEN, LADEN }
-export(SpriteMode) var sprite_mode = SpriteMode.UNLADEN
 
 var flap: bool = false
 var move: Vector2 = Vector2(0,0)
@@ -25,20 +25,43 @@ func _input(event):
 
 func _physics_process(delta):
 	if flap:
+		mode = MODE_RIGID
+		$Wave.stop_all()
 		linear_damp = -1
 		.apply_central_impulse(FLAP_VEC)
 		flap = false
 		return
 	
-	.apply_central_impulse(move)
-	.apply_central_impulse(G)
+	if position.x < 0:
+		.apply_central_impulse(Vector2(10, 0))
+	elif position.x > get_viewport().size.x:
+		.apply_central_impulse(Vector2(-10, 0))
+	else:
+		.apply_central_impulse(move)
+		.apply_central_impulse(G)
+	
 	move = Vector2(0,0)
-
 
 func _integrate_forces(state):
 	state.linear_velocity = Vector2(
 								clamp(self.linear_velocity[0], -MAX_VEL, MAX_VEL),
 								clamp(self.linear_velocity[1], -MAX_VEL, MAX_VEL)
 							)
-	if position.y >= get_viewport().size.y:
-		linear_damp = 100
+	if position.y >= get_viewport().size.y - 15: #Keep us above the water
+		if !mode == MODE_KINEMATIC:
+			mode = MODE_KINEMATIC
+			linear_damp = 100
+			tween_start_pos = self.position
+			_start_tween(tween_start_pos, tween_start_pos + amplitude)
+
+func _on_Bottle_pickup():
+	$Sprite.visible = !$Sprite.visible
+	$Sprite2.visible = !$Sprite2.visible
+
+func _start_tween(start, end):
+	$Wave.interpolate_property(self, "position", start, end, Tween.TRANS_SINE, 1)
+	$Wave.start()
+
+func _on_Wave_tween_completed(object, key):
+	amplitude[1] *= -1
+	_start_tween(null, self.position + amplitude)
